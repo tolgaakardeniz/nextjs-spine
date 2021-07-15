3dab72482138bcb28afdded8fd1ef2d7becce696
Tolga AKARDENİZ <tolga.akardeniz@hotmail.com.tr>
2021-07-15 21:06:17 +0300

    First Upload

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>About Us</title>
+      </Head>
+      <div className="text-center">Abaut</div>
+    </>
+  );
+}
